5731372f13868c6eea5feaaea3aa7603457c6a70
Chris Kite <chris@chriskite.com>
2015-08-05 13:33:04 -0500

    updates for offers

--- pure-ftpd.sh
+++ pure-ftpd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/sbin/pure-ftpd -l pam -l puredb:/etc/pure-ftpd/pureftpd.pdb 1000 -8 UTF-8 -E
+exec /usr/sbin/pure-ftpd -l pam -l puredb:/etc/pure-ftpd/pureftpd.pdb 1000 -8 UTF-8 -E -p 49000:49100 -P 54.173.183.157
